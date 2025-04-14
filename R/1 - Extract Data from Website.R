e743927f362d4842e726afaa51b2cc0b74165104
vmijat <vmijatovic@gmail.com>
2025-04-14 13:20:19 +0200

    Add folders; add first version on data extraction

---
+++ R/1 - Extract Data from Website.R
@@ -0,0 +1,11 @@
+# Load Libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(rvest)
+
+
+
+# Connect to URL ----------------------------------------------------------
+
+url <- "https://www.forbes.com/sites/brettknight/2024/12/18/the-worlds-highest-paid-female-athletes-2024/"
+
